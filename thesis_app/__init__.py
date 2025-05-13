f60324a394cc2293671698bb0659feaa677a2784
acimino <cimino.antonio@gmail.com>
2025-05-13 19:40:16 +0200

    Initial commit

---
+++ thesis_app/__init__.py
@@ -0,0 +1,1 @@
+"""thesis-app: A Flower / PyTorch app."""
---
+++ thesis_app/client_app.py
@@ -0,0 +1,100 @@
+import torch
+
+from flwr.client import ClientApp, NumPyClient,Client
+from flwr.common import Context
+from thesis_app.task import Net, get_weights, load_data, set_weights, test, train
+from flwr.common import (
+    Code,
+    EvaluateIns,
+    EvaluateRes,
+    FitIns,
+    FitRes,
+    GetParametersIns,
+    GetParametersRes,
+    Status,
+    ndarrays_to_parameters,
+    parameters_to_ndarrays,
+)
+
+
+class FlowerClient(Client):
+    def __init__(self, net, trainloader, valloader, local_epochs):
+        self.net = net
+        self.trainloader = trainloader
+        self.valloader = valloader
+        self.local_epochs = local_epochs
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.net.to(self.device)
+
+    def get_parameters(self, ins: GetParametersIns) -> GetParametersRes:
+
+        # Get parameters as a list of NumPy ndarray's
+        ndarrays: List[np.ndarray] = get_weights(self.net)
+
+        # Serialize ndarray's into a Parameters object
+        parameters = ndarrays_to_parameters(ndarrays)
+
+        # Build and return response
+        status = Status(code=Code.OK, message="Success")
+        return GetParametersRes(
+            status=status,
+            parameters=parameters,
+        )
+
+    def fit(self, ins: FitIns) -> FitRes:
+
+        # Deserialize parameters to NumPy ndarray's
+        parameters_original = ins.parameters
+        ndarrays_original = parameters_to_ndarrays(parameters_original)
+
+        # Update local model, train, get updated parameters
+        set_weights(self.net, ndarrays_original)
+        train(self.net, self.trainloader, epochs=1,device=self.device)
+        ndarrays_updated = get_weights(self.net)
+
+        # Serialize ndarray's into a Parameters object
+        parameters_updated = ndarrays_to_parameters(ndarrays_updated)
+
+        # Build and return response
+        status = Status(code=Code.OK, message="Success")
+        return FitRes(
+            status=status,
+            parameters=parameters_updated,
+            num_examples=len(self.trainloader),
+            metrics={},
+        )
+
+    def evaluate(self, ins: EvaluateIns) -> EvaluateRes:
+
+        # Deserialize parameters to NumPy ndarray's
+        parameters_original = ins.parameters
+        ndarrays_original = parameters_to_ndarrays(parameters_original)
+
+        set_weights(self.net, ndarrays_original)
+        loss, accuracy = test(self.net, self.valloader, self.device)
+
+        # Build and return response
+        status = Status(code=Code.OK, message="Success")
+        return EvaluateRes(
+            status=status,
+            loss=float(loss),
+            num_examples=len(self.valloader),
+            metrics={"accuracy": float(accuracy)},
+        )
+
+
+
+def client_fn(context: Context):
+    # Load model and data
+    net = Net()
+    partition_id = context.node_config["partition-id"]
+    num_partitions = context.node_config["num-partitions"]
+    trainloader, valloader = load_data(partition_id, num_partitions)
+    local_epochs = context.run_config["local-epochs"]
+
+    # Return Client instance
+    return FlowerClient(net, trainloader, valloader, local_epochs).to_client()
+
+
+# Create the ClientApp
+app = ClientApp(client_fn=client_fn)
